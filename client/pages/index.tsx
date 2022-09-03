02743e3bd9c78fe367d6141856502599d1ae0db1
Mathias Basire <mb@vectahosting.eu>
2022-09-03 04:14:04 +0200

    Test 2

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -29,7 +29,6 @@ const Homepage = () => {
       {!isAuthenticated && <NeedToLogin />}
       {isAuthenticated && <LinksTable />}
       <Features />
-/*      <Extensions /> */
       <Footer />
     </AppWrapper>
   );
